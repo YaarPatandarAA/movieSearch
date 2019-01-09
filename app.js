6ad43e114a9a169cb066b00f0bb473f278645c0a
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-08 21:48:10 -0800

    add search page

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const request = require("request");
 app.set("view engine", "ejs")
 var apiFile = require('./api.js');
 
+app.get("/", function (req, res){
+    res.render("search");
+});
+
 app.get("/results", function(req, res) {
     request("http://www.omdbapi.com/?apikey=" + apiFile.api +"&s=star", function (error, response, body) {
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
-            res.render("results", {data: data})
+            res.render("results", {data: data});
         }
     });
 });
