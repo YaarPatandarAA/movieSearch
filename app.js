ed82ec20a1e997dd5ab1ca224409a2fde37b9ddb
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-08 21:34:29 -0800

    initial API call

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const port = 3000;
 const request = require("request");
 
+var apiFile = require('./api.js');
+
 app.get("/results", function(req, res) {
-    res.send("Hello, it works!");
+    request("http://www.omdbapi.com/?apikey=" + apiFile.api +"&s=star", function (error, response, body) {
+        if(!error && response.statusCode == 200){
+            var results = JSON.parse(body);
+            res.send(results["Search"][0]["Title"]);
+        }
+    });
 });
 
 app.get("*", function(req, res) {
